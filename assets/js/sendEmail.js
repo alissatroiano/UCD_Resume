f00eeb99ecf5d4570206a62fecd7932cbb89efb1
Alissa Troiano <alissatroianonyc@gmail.com>
2020-08-09 19:32:38 +0000

    Create script for emailjs service

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+function sendMail(contactForm) {
+  emailjs
+    .send("gmail", "student", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+
+  return false;
+}
